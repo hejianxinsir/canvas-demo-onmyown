a68fa8892fbaa82e6b73b1fc6bceb29f309533af
hejianxin <hejianxinsir@gmail.com>
2019-07-05 20:05:51 +0800

    画板基本完成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+// document.body.ontouchstart = function(eee){
+//   eee.preventDefault()
+// }
+
 var yyy = document.getElementById('canvas');
 var context = yyy.getContext('2d');
+var lineWidth = 3
 
 autoSetCanvasSize(yyy)
 
@@ -7,15 +12,81 @@ listenToUser(yyy)
 
 
 var eraserEnabled = false
-eraser.onclick = function() {
-  eraserEnabled =true
-  actions.className = 'actions x'
-  
-}
-brush.onclick = function(){
+pen.onclick = function(){  //点 pen 的时候给自己一个类
   eraserEnabled = false
-  actions.className = 'actions'
+  pen.classList.add('active')
+  eraser.classList.remove('active')
 }
+eraser.onclick = function(){
+  eraserEnabled = true
+  eraser.classList.add('active')
+  pen.classList.remove('active')
+}
+
+// 画笔颜色
+black.onclick = function(){
+  context.fillStyle = 'black'
+  context.strokeStyle = 'black'
+  black.classList.add('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+  yellow.classList.remove('active')
+}
+red.onclick = function(){
+  context.fillStyle = 'red'
+  context.strokeStyle = 'red'
+  black.classList.remove('active')
+  red.classList.add('active')
+  green.classList.remove('active')
+  yellow.classList.remove('active')
+}
+green.onclick = function(){
+  context.strokeStyle = 'green'
+  black.classList.remove('active')
+  red.classList.remove('active')
+  green.classList.add('active')
+  yellow.classList.remove('active')
+}
+yellow.onclick = function(){
+  context.strokeStyle = 'yellow'
+  black.classList.remove('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+  yellow.classList.add('active')
+}
+
+thin.onclick = function(){
+  lineWidth = 3
+}
+medium.onclick = function(){
+  lineWidth = 7
+}
+thick.onclick = function(){
+  lineWidth = 12
+}
+
+clear.onclick = function(){
+  context.clearRect(0, 0, yyy.width, yyy.height); //画一个空白大方形就行了
+}
+
+download.onclick = function(){
+  var url = yyy.toDataURL("image/png")
+  var a = document.createElement('a')
+  document.body.appendChild(a)
+  a.href = url
+  a.download = '我的画作'
+  a.target = '_blank'
+  a.click()
+}
+// eraser.onclick = function() {
+//   eraserEnabled =true
+//   actions.className = 'actions x'
+  
+// }
+// brush.onclick = function(){
+//   eraserEnabled = false
+//   actions.className = 'actions'
+// }
 
 
 /******/
@@ -38,16 +109,16 @@ function autoSetCanvasSize(canvas) {
 
 function drawCircle(x, y, radius) {
   context.beginPath()
-  context.fillStyle = 'black'
+  // context.fillStyle = 'black'
   context.arc(x, y, radius, 0, Math.PI * 2);
   context.fill()
 }
 
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'black'
+  // context.strokeStyle = 'black'
   context.moveTo(x1, y1) // 起点
-  context.lineWidth = 5
+  context.lineWidth = lineWidth  //原始画笔的粗细是 5px 
   context.lineTo(x2, y2) // 终点
   context.stroke()
   context.closePath()
@@ -139,8 +210,5 @@ function listenToUser(canvas) {
       using = false
     }
   }
-
-
-
 
 }
